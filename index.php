7fea31f74b6787c190af4f6b3e7ccf44ae2d2676
Alex Kunze <alex.kunze13@gmail.com>
2023-10-28 21:42:24 +0200

    adding styles

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ session_start();
     <link rel="stylessheet" href="styles.css">
 </head>
 <body>
+<header>
+    <img src = "branding/logo_large.png">
+</header>
     <h1>
         Testside
     </h1>
