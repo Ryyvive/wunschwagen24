284fe5e37e4402e185bd85a7c3a20e8f31eb4674
Alex Kunze <alex.kunze13@gmail.com>
2023-10-28 18:21:16 +0200

    start of something

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 
+<?php
+session_start();
+?>
+<html>
+<head>
+    <title>Wunschwagen 24 - Dein Gebrauchtwagen</title>
+    <link rel="stylessheet" href="styles.css">
+</head>    
+</html>
---
+++ sql.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+    <title>Wunschwagen 24 - Dein Gebrauchtwagen</title>
+    <link rel="stylessheet" href="styles.css">
+</head>    
+</html>
