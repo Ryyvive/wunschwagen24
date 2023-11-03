8926c107e7c571e03aa4cc8b43c955449f4164a0
alexk <alex.kunze13@gmail.com>
2023-11-03 08:08:45 +0100

    frühmorgenpush 03.11.2023

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])){
                 <p class="page-link">
                     <span class="page-link-label">Passwort vergessen?</span>
                 </p>
-                <input type="submit" class="form-btn" formaction="submit">Log in</input>
+                <input type="submit" class="form-btn" formaction="submit" value = "Log in">
             </form>
             <p class="sign-up-label">
                 Noch keinen Account?<span class="sign-up-link"><a href="register.php">Registrieren</a></span>
