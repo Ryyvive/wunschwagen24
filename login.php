a6ba5663a2b416beed016f57d50cca3d7202ef33
alexk <alex.kunze13@gmail.com>
2023-11-03 09:13:26 +0100

    frühmorgenpush 03.11.2023

--- login.php
+++ login.php
@@ -15,7 +15,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])){
 
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     $res = sqlsrv_query($conn, $sql_check_mail);
-    echo sqlsrv_num_rows($res);
+    echo $res;
     #if(sqlsrv_num_rows($res) == 1){
     #    $valid = true;
     #    $login = true;
