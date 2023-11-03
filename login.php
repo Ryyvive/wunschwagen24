7f747acc8871e99de61fa7e431199d76384e0e18
alexk <alex.kunze13@gmail.com>
2023-11-03 07:36:30 +0100

    frühmorgenpush 03.11.2023

--- login.php
+++ login.php
@@ -1,5 +1,30 @@
 <?php
 session_start();
+$valid = true;
+$login = false;
+if (isset($_POST["email"]) && isset($_POST["password"])){
+    $email = htmlspecialchars($_POST["email"]);
+    $password_user = htmlspecialchars($_POST["password"]);
+
+    $sql_check_mail = "SELECT vorname FROM customer WHERE email = '".$email. "' and WHERE password = ".$password_user;
+    $serverName = "wunschwagen24-dbserver-dev.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "wunschwagen-db-dev", // update me
+        "Uid" => "CloudSA1cb8415e", // update me
+        "PWD" => "340Uuxwp7Mcxo7Khy" // update me
+    );
+// Create connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $res = sqlsrv_query($conn, $sql_check_mail);
+    if($res->num_rows == 1){
+        $valid = true;
+        $login = true;
+        header("Location: index.php");
+    }else{
+        $valid = false;
+    }
+
+}
 ?>
 
 <html lang="DE">
@@ -15,17 +40,20 @@ session_start();
             </a>
             <div class="breaker"></div>
         <div class="form-container">
-              <form class="login-form">
+              <form class="login-form" method="post" action="login.php">
                   <span style="margin: auto"><h1>WunschWagen24 Login</h1></span>
-                <input type="email" class="input" placeholder="Email">
+                  <?PHP if(!$valid){
+                  echo '<span style="margin: auto color :red">Email oder Passwort sind incorrect</span>';
+                  } ?>
+                <input type="email" class="input" placeholder=<?php if(isset($email)){echo $email;}else{echo "Email";}?>>
                 <input type="password" class="input" placeholder="Password">
                 <p class="page-link">
                     <span class="page-link-label">Passwort vergessen?</span>
                 </p>
-                <button class="form-btn">Log in</button>
+                <button class="form-btn" formaction="submit">Log in</button>
             </form>
             <p class="sign-up-label">
-                Noch keinen Account?<span class="sign-up-link">Registrieren</span>
+                Noch keinen Account?<span class="sign-up-link"><a href="register.php">Registrieren</a></span>
             </p>
         </div>
         </div>
---
+++ register.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+$valid = true;
+$login = false;
+if (isset($_POST["email"]) && isset($_POST["vorname"]) && isset($_POST["nachname"]) && isset($_POST["password"])){
+    $vorname = htmlspecialchars($_POST["vorname"]);
+    $nachname = htmlspecialchars($_POST["nachname"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $password_user = htmlspecialchars($_POST["password"]);
+
+    $sql_check_mail = "SELECT email FROM customer WHERE email = '".$email. "'";
+    $serverName = "wunschwagen24-dbserver-dev.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "wunschwagen-db-dev", // update me
+        "Uid" => "CloudSA1cb8415e", // update me
+        "PWD" => "340Uuxwp7Mcxo7Khy" // update me
+    );
+
+// Create connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $res = sqlsrv_query($conn, $sql_check_mail);
+    if($res->num_rows == 1){
+        $valid = false;
+        $login = false;
+    }else{
+        //zu DATENBANK HINZUFÜGEN
+    }
+
+}
+?>
+
+<html lang="DE">
+<head>
+    <title>Anmeldung</title>
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="icon" href="branding/logo_small_icon_only.png">
+</head>
+<body>
+<div class = main-content style="background: transparent">
+    <a href="index.php">
+        <img src="branding/logo_small_icon_only_inverted.png">
+    </a>
+    <div class="breaker"></div>
+    <div class="form-container">
+        <form class="login-form">
+            <span style="margin: auto"><h1>WunschWagen24 Login</h1></span>
+            <input required id="email" name="email" type="email" class="input" placeholder="Email">
+            <input required id="vorname" name="vorname" type="text" class = "input" placeholder="Vorname">
+            <input required id="nachname" name="nachname" type="text" class = "input" placeholder="Nachname">
+            <input required id="password" name="password" type="password" class="input" placeholder="Password">
+            <p class="page-link">
+                <span class="page-link-label">Passwort vergessen?</span>
+            </p>
+            <button class="form-btn" formaction="submit">Sign Up</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
