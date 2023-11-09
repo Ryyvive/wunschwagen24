e43851c324e11074e04d6e3e55b1182640fb882a
alexk <alex.kunze13@gmail.com>
2023-11-09 10:09:34 +0100

    trying sql logic

--- search.php
+++ search.php
@@ -1,5 +1,15 @@
 <?php
 session_start();
+
+$serverName = "wunschwagen24-dbserver-dev.database.windows.net";
+$connectionOptions = array(
+    "Database" => "wunschwagen-db-dev",
+    "Uid" => "CloudSA1cb8415e",
+    "PWD" => "340Uuxwp7Mcxo7Khy"
+);
+$conn = sqlsrv_connect($serverName, $connectionOptions)
+
+#$res = sqlsrv_query($conn, <statenment>);
 ?>
 
 <html lang="DE">
@@ -12,6 +22,18 @@ session_start();
     <header></header>
     <div class="breaker"></div>
     <div class="main-content">
+        <form class = "search" method = "POST" action="search.php">
+            <select id="brand" name="brand">
+                <?php
+                    $sqlstatement = "SELECT DISTICT brand from cars";
+                    $res = sqlsrv_query($conn, $sqlstatement);
+                    while( $row = sqlsrv_fetch_array( $res, SQLSRV_FETCH_ASSOC) ) {
+                        echo "<option value =".$row["brand"].">".$row["brand"]."</option>";
+                    }
+                ?>
+            </select>>
+
+        </form>
         
     </div>
 </body>
