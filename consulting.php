ded47374013fe28f7572f530072565c3ff1e2670
alexk <alex.kunze13@gmail.com>
2023-12-15 13:56:32 +0100

    adding proiles

--- consulting.php
+++ consulting.php
@@ -1,2 +1,58 @@
 <?php
 session_start();
+?>
+<html lang="DE">
+<head>
+    <title>Wunschwagen 24 - Dein Gebrauchtwagen</title>
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="icon" href="branding/logo_small_icon_only.png">
+</head>
+<body>
+<header>
+    <?php include("header.php") ?>
+</header>
+<div class="teaminfo">
+    <table>
+        <tr>
+            <td>
+                <img src="team/Kunze.jpeg" alt="">
+            </td>
+            <td>
+                Herr Kunze ist Berater bei uns seit 2023!
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="team/Fey.jpeg" alt="">
+            </td>
+            <td>
+                Herr Fey ist Berater bei uns seit 2023!
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="team/Barz.jpeg" alt="">
+            </td>
+            <td>
+                Herr Barz ist Berater bei uns seit 2023!
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="team/Hartemann.jpeg" alt="">
+            </td>
+            <td>
+                Herr Hartemann ist Berater bei uns seit 2023!
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="team/Marzusch.jpeg" alt="">
+            </td>
+            <td>
+                Herr Marzusch ist Berater bei uns seit 2023!
+            </td>
+        </tr>
+    </table>
+</div>
+</html>
