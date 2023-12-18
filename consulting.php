028819838398e09b95eaafd928050ebc8c02e8c8
Dominik Fey <dominik.fey@telekom.de>
2023-12-18 14:43:39 +0100

    Changed Consulting Page Layout, Content and CSS

--- consulting.php
+++ consulting.php
@@ -13,50 +13,87 @@ session_start();
 </header>
 <div class="breaker"></div>
 <div class="main-content">
-<div class="teaminfo">
-    <table>
-        <tr>
-            <td>
-                <img src="team/Kunze.jpeg" alt="">
-            </td>
-            <td>
-                Herr Kunze ist Berater bei uns seit 2023!
-                <button>Hier einen Termin vereinbaren</button>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <img src="team/Fey.jpeg" alt="">
-            </td>
-            <td>
-                Herr Fey ist Berater bei uns seit 2023!
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <img src="team/Barz.jpeg" alt="">
-            </td>
-            <td>
-                Herr Barz ist Berater bei uns seit 2023!
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <img src="team/Hartemann.jpeg" alt="">
-            </td>
-            <td>
-                Herr Hartemann ist Berater bei uns seit 2023!
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <img src="team/Marzusch.jpeg" alt="">
-            </td>
-            <td>
-                Herr Marzusch ist Berater bei uns seit 2023!
-            </td>
-        </tr>
-    </table>
-</div>
+    <div class="teaminfo">
+        <h1>Möchten Sie zusätzlich zur KI noch weitere Expertise? Lassen Sie sich von unseren Experten beraten!</h1><br>
+        <table>
+            <tr>
+                <td>
+                    <img src="team/Kunze.jpeg" alt="">
+                </td>
+                <td>
+                    <span class="highlight-name">Herr Kunze</span>
+                    ist Berater bei uns seit
+                    <span class="highlight-year">2023</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img src="team/Fey.jpg" alt="">
+                </td>
+                <td>
+                    <span class="highlight-name">Herr Fey</span>
+                    ist Berater bei uns seit <br>
+                    <span class="highlight-year">2023</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img src="team/Barz.jpeg" alt="">
+                </td>
+                <td>
+                    <span class="highlight-name">Herr Barz</span>
+                    ist Berater bei uns seit <br>
+                    <span class="highlight-year">2023</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img src="team/Hartemann.jpeg" alt="">
+                </td>
+                <td>
+                    <span class="highlight-name">Herr Hartemann</span>
+                    ist Berater bei uns seit <br>
+                    <span class="highlight-year">2023</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img src="team/Marzusch.jpeg" alt="">
+                </td>
+                <td>
+                    <span class="highlight-name">Herr Marzusch</span>
+                    ist Berater bei uns seit <br>
+                    <span class="highlight-year">2023</span>
+                </td>
+            </tr>
+        </table><br><br>
+    </div>
+    <div class="contact-form">
+        <h2>Termin vereinbaren</h2>
+        <form method="post" action="consulting.php">
+            <label for="berater">Wähle einen Berater:</label>
+            <select name="berater" id="berater">
+                <option value="kunze">Herr Kunze</option>
+                <option value="fey">Herr Fey</option>
+                <option value="barz">Herr Barz</option>
+                <option value="hartemann">Herr Hartemann</option>
+                <option value="marzusch">Herr Marzusch</option>
+            </select>
+
+            <label for="name">Dein Name:</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="email">Deine E-Mail:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="date">Wunschtermin:</label>
+            <input type="date" id="date" name="date" required>
+
+            <label for="message">Nachricht:</label>
+            <textarea id="message" name="message"></textarea>
+
+            <input type="submit" value="Termin anfragen">
+        </form>
+    </div>
 </div>
 </html>
